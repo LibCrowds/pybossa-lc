27f78db24e2210a6b882e9bc4a68025312e31a14
Alex Mendes <alexanderhmendes@gmail.com>
2017-11-28 18:33:52 +0000

    Add old tests

--- test/__init__.py
+++ test/__init__.py
@@ -7,12 +7,15 @@ import pybossa_lc as plugin
 # Use the PyBossa test suite
 sys.path.append(os.path.abspath("./pybossa/test"))
 
-from default import Test
+
+PYBOSSA_TEST_SETTINGS='pybossa/settings_test.py'
 
 
 def setUpPackage():
     """Setup the plugin."""
     from default import flask_app
     with flask_app.app_context():
+        settings = os.path.abspath(PYBOSSA_TEST_SETTINGS)
+        flask_app.config.from_pyfile(settings)
         plugin_dir = os.path.dirname(plugin.__file__)
         plugin.PyBossaLC(plugin_dir).setup()
