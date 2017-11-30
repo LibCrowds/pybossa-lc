ee3b3f205c97e8f870b6db699f973d3d919154cd
Alexander Mendes <alexanderhmendes@gmail.com>
2017-11-30 14:43:41 +0000

    Improve comments for z3950 analysis and use last_version

--- pybossa_lc/analysis/z3950.py
+++ pybossa_lc/analysis/z3950.py
@@ -14,32 +14,37 @@ VALID_KEYS = ['oclc', 'shelfmark', 'comments']
 def analyse(result_id):
     """Analyse Z39.50 results."""
     result = result_repo.get(result_id)
+
+    # Filter the valid task run keys
     df = helpers.get_task_run_df(result.task_id)
     df = df.loc[:, df.columns.isin(VALID_KEYS)]
+
+    # Initialise the result with empty values for each task run key
+    result.info = {k: "" for k in df.keys()}
+
+    # Check if there are any non-empty answers
     df = helpers.drop_empty_rows(df)
-    n_task_runs = len(df.index)
-
-    # Initialise the result
-    defaults = {k: "" for k in df.keys()}
-    defaults['analysis_complete'] = True
-    result.info = dict(defaults)
-
     has_answers = not df.empty
+
+    # Check if the match percentage is met
+    n_task_runs = len(df.index)
     has_matches = helpers.has_n_matches(df, n_task_runs, MATCH_PERCENTAGE)
 
-    # Matching answers
+    # Store the matching result if match percentage met
     if has_answers and has_matches:
         for k in df.keys():
             result.info[k] = df[k].value_counts().idxmax()
 
-    # Varied answers
+    # Mark for further checking if match percentage not met
     elif has_answers:
-        result.info['analysis_complete'] = False
+        result.last_version = False
+
     result_repo.save(result)
 
 
 def analyse_all(project_id):
     """Analyse all Z39.50 results."""
+    project = project_repo.get(project_id)
     results = result_repo.filter_by(project_id=project_id)
     for result in results:
         analyse(result)
