bf1137ea10cd39132f51df256e099795e1d83cf6
Alexander Mendes <alexanderhmendes@gmail.com>
2017-12-11 15:54:25 +0000

    Add enhance task data from parent

--- pybossa_lc/importers/iiif.py
+++ pybossa_lc/importers/iiif.py
@@ -2,6 +2,7 @@
 """IIIF importer module for pybossa-lc"""
 import requests
 from pybossa.importers.base import BulkTaskImport
+from pybossa.core import result_repo
 
 
 class BulkTaskIIIFImporter(BulkTaskImport):
@@ -9,10 +10,11 @@ class BulkTaskIIIFImporter(BulkTaskImport):
 
     importer_id = "iiif-annotation"
 
-    def __init__(self, manifest_uri, template):
+    def __init__(self, manifest_uri, template, parent_id=None):
         """Init method."""
         self.manifest_url = manifest_uri
         self.template = template
+        self.parent_id = parent_id
 
     def tasks(self):
         """Get tasks."""
@@ -26,6 +28,9 @@ class BulkTaskIIIFImporter(BulkTaskImport):
         """Generate the tasks."""
         manifest = requests.get(self.manifest_url).json()
         task_data = self._get_task_data_from_manifest(manifest)
+        if self.parent_id:
+            task_data = self._enhance_task_data_from_parent(task_data,
+                                                            self.parent_id)
         return [dict(info=data) for data in task_data]
 
     def _get_task_data_from_manifest(self, manifest):
@@ -57,6 +62,58 @@ class BulkTaskIIIFImporter(BulkTaskImport):
                 }
             data.append(row)
         return data
+
+    def _enhance_task_data_from_parent(self, task_data, parent_id):
+        """Add tasks according to the results of a parent task."""
+        indexed_task_data = {row['target']: row for row in task_data}
+        results = result_repo.filter_by(project_id=parent_id)
+        enhanced_task_data = []
+        for row in results:
+            info = row['info']
+
+            if not info:
+                raise ValueError('The info field for a result is empty')
+            annotations = info['annotations']
+
+            for anno in annotations:
+                if anno['motivation'] == 'tagging':
+                    source = anno['target']['source']
+                    selector = anno['target']['selector']['value']
+                    rect = selector.split('=')[1].split(',')
+                    data = indexed_task_data[source].copy()
+                    data['highlights'] = [
+                        {
+                            'x': float(rect[0]),
+                            'y': float(rect[1]),
+                            'width': float(rect[2]),
+                            'height': float(rect[3])
+                        }
+                    ]
+                    data['bounds'] = {
+                        'x': float(rect[0]) + data['bounds']['x'],
+                        'y': float(rect[1]) + data['bounds']['y'],
+                        'width': float(rect[2]) + data['bounds']['width'],
+                        'height': float(rect[3]) + data['bounds']['height']
+                    }
+                    data['parent_task_id'] = row['task_id']
+                    enhanced_task_data.append(data)
+
+                elif anno['motivation'] == 'describing':
+                    source = anno['target']['source']
+                    data = indexed_task_data[source].copy()
+                    data['parent_task_id'] = row['task_id']
+                    enhanced_task_data.append(data)
+
+                elif anno['motivation'] != 'commenting':
+                    raise ValueError('Unknown motivation')
+
+        # Sort
+        return sorted(enhanced_task_data,
+                      key=lambda x: (
+                          x['target'],
+                          x['highlights'][0]['y'],
+                          x['highlights'][0]['x']
+                      ))
 
     def _get_share_url(self, manifest_url, canvas_index):
         """Return a Universal Viewer URL for sharing."""
