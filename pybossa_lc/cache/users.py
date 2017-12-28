b9da61f20ffdd99be47d4afedc28d3d9e3021aaa
Alex Mendes <alexanderhmendes@gmail.com>
2017-12-28 23:49:57 +0000

    Fix get coowned templates

--- pybossa_lc/cache/users.py
+++ pybossa_lc/cache/users.py
@@ -13,14 +13,13 @@ session = db.slave_session
 @memoize(timeout=timeouts.get('USER_TIMEOUT'))
 def get_user_templates(user_id):
     """Return templates that the user owns or co-owns."""
-    sql = text('''SELECT info->>'templates' AS templates,
-                  json_array_elements((info->>'templates')::jsonb) AS bar
+    sql = text('''SELECT info->>'templates' AS templates
                   FROM "user"
+                  WHERE id = :user_id
+                  OR (info->>'templates')::jsonb @> '[{"project": {"coowners": [:user_id]}}]'
                   ''')
-    print sql
     result = session.execute(sql, dict(user_id=user_id))
     templates = []
     for row in result:
-        print row
         templates += json.loads(row.templates) if row.templates else []
     return templates
