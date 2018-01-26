7c995dad9c4e4bb54dbc75d56c4dcb2e92006c39
Alexander Mendes <alexanderhmendes@gmail.com>
2018-01-26 17:55:17 +0000

    Update tests

--- test/test_analysis/test_z3950.py
+++ test/test_analysis/test_z3950.py
@@ -5,9 +5,12 @@ import numpy
 import pandas
 from mock import patch, call
 from factories import TaskFactory, TaskRunFactory, ProjectFactory
+from factories import CategoryFactory, UserFactory
 from default import Test, with_context, db
 from pybossa.repositories import ResultRepository
+from nose.tools import *
 
+from ..fixtures import TemplateFixtures
 from pybossa_lc.analysis import z3950
 
 
@@ -20,7 +23,8 @@ class TestZ3950Analysis(Test):
     @with_context
     def test_empty_results(self):
         """Test that an empty result is updated correctly."""
-        task = TaskFactory.create(n_answers=1)
+        project = ProjectFactory.create()
+        task = TaskFactory.create(n_answers=1, project=project)
         TaskRunFactory.create(task=task, info={
             'control_number': '',
             'reference': '',
@@ -28,17 +32,18 @@ class TestZ3950Analysis(Test):
         })
         result = self.result_repo.filter_by(project_id=task.project_id)[0]
         z3950.analyse(result.id)
-        assert result.last_version, "last_version should be True"
-        assert result.info == {
+        assert_equal(result.last_version, True)
+        assert_dict_equal(result.info, {
             'control_number': '',
             'reference': '',
             'comments': ''
-        }, "info should be empty values for all keys"
+        })
 
     @with_context
     def test_results_with_deprecated_keys(self):
         """Test that the old Convert-a-Card keys are converted."""
-        task = TaskFactory.create(n_answers=1)
+        project = ProjectFactory.create()
+        task = TaskFactory.create(n_answers=1, project=project)
         TaskRunFactory.create(task=task, info={
             'oclc': '',
             'shelfmark': '',
@@ -46,34 +51,46 @@ class TestZ3950Analysis(Test):
         })
         result = self.result_repo.filter_by(project_id=task.project_id)[0]
         z3950.analyse(result.id)
-        assert result.info == {
+        assert_dict_equal(result.info, {
             'control_number': '',
             'reference': '',
             'comments': ''
-        }, "keys should be updated to the new names"
+        })
 
     @with_context
     def test_results_with_comments(self):
         """Test that results with comments are updated correctly."""
-        task = TaskFactory.create(n_answers=1)
+        project = ProjectFactory.create()
+        task = TaskFactory.create(n_answers=3, project=project)
         TaskRunFactory.create(task=task, info={
-            'control_number': '',
-            'reference': '',
+            'control_number': '123',
+            'reference': '456',
             'comments': 'Some comment'
+        })
+        TaskRunFactory.create(task=task, info={
+            'control_number': '123',
+            'reference': '456',
+            'comments': ''
+        })
+        TaskRunFactory.create(task=task, info={
+            'control_number': '123',
+            'reference': '456',
+            'comments': ''
         })
         result = self.result_repo.filter_by(project_id=task.project_id)[0]
         z3950.analyse(result.id)
-        assert not result.last_version, "last_version should be False"
-        assert result.info == {
+        assert_equal(result.last_version, False)
+        assert_dict_equal(result.info, {
             'control_number': '',
             'reference': '',
             'comments': ''
-        }, "info should be empty values for all keys"
+        })
 
     @with_context
     def test_results_with_matching_answers(self):
         """Test that results with matching answers are updated correctly."""
-        task = TaskFactory.create(n_answers=3)
+        project = ProjectFactory.create()
+        task = TaskFactory.create(n_answers=3, project=project)
         TaskRunFactory.create(task=task, info={
             'control_number': '123',
             'reference': 'abc',
@@ -91,17 +108,18 @@ class TestZ3950Analysis(Test):
         })
         result = self.result_repo.filter_by(project_id=task.project_id)[0]
         z3950.analyse(result.id)
-        assert result.last_version, "last_version should be True"
-        assert result.info == {
+        assert_equal(result.last_version, True)
+        assert_dict_equal(result.info, {
             'control_number': '123',
             'reference': 'abc',
             'comments': ''
-        }, "control_number and reference should be populated"
+        })
 
     @with_context
     def test_results_with_non_matching_answers(self):
         """Test results with non-matching answers are updated correctly."""
-        task = TaskFactory.create(n_answers=3)
+        project = ProjectFactory.create()
+        task = TaskFactory.create(n_answers=3, project=project)
         TaskRunFactory.create(task=task, info={
             'control_number': '123',
             'reference': 'abc',
@@ -119,12 +137,12 @@ class TestZ3950Analysis(Test):
         })
         result = self.result_repo.filter_by(project_id=task.project_id)[0]
         z3950.analyse(result.id)
-        assert not result.last_version, "last_version should be False"
-        assert result.info == {
+        assert_equal(result.last_version, False)
+        assert_dict_equal(result.info, {
             'control_number': '',
             'reference': '',
             'comments': ''
-        }, "info should be empty values for all keys"
+        })
 
     @with_context
     @patch('pybossa_lc.analysis.z3950.analyse', return_value=True)
@@ -154,3 +172,38 @@ class TestZ3950Analysis(Test):
         self.result_repo.update(results[0])
         z3950.analyse_empty(project.id)
         mock_analyse.assert_called_once_with(results[1].id)
+
+    @with_context
+    def test_references_are_normalised(self):
+        """Test references are normalised according to set analysis rules."""
+        category = CategoryFactory()
+        tmpl_fixtures = TemplateFixtures(category)
+        tmpl = tmpl_fixtures.create_template()
+        tmpl['rules'] = dict(case='title', whitespace='full_stop',
+                             trim_punctuation=True)
+        UserFactory.create(info=dict(templates=[tmpl]))
+        project = ProjectFactory.create(info=dict(template_id=tmpl['id']))
+        task = TaskFactory.create(n_answers=3, project=project)
+        TaskRunFactory.create(task=task, info={
+            'control_number': '123',
+            'reference': 'OR 123  456.',
+            'comments': ''
+        })
+        TaskRunFactory.create(task=task, info={
+            'control_number': '123',
+            'reference': 'Or.123.456. ',
+            'comments': ''
+        })
+        TaskRunFactory.create(task=task, info={
+            'control_number': '456',
+            'reference': 'or 123 456',
+            'comments': ''
+        })
+        result = self.result_repo.filter_by(project_id=task.project_id)[0]
+        z3950.analyse(result.id)
+        assert_equal(result.last_version, True)
+        assert_dict_equal(result.info, {
+            'control_number': '123',
+            'reference': 'Or.123.456',
+            'comments': ''
+        })
