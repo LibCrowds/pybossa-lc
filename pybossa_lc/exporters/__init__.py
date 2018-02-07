fe91ab40e23636197b75cd511903614e33cf3bf2
Alexander Mendes <alexanderhmendes@gmail.com>
2018-02-07 16:35:10 +0000

    Overwrite the download name method for volumes

--- pybossa_lc/exporters/__init__.py
+++ pybossa_lc/exporters/__init__.py
@@ -2,6 +2,7 @@
 """Volume exporter module for pybossa-lc."""
 
 from pybossa.exporter import Exporter
+from werkzeug.utils import secure_filename
 
 
 class VolumeExporter(Exporter):
@@ -11,3 +12,10 @@ class VolumeExporter(Exporter):
 
     def _container(self, volume):
         return "category_{}".format(volume.category_id)
+
+    def download_name(self, volume, ty, _format):
+        """Overwrite the download name method for volumes."""
+        name = self._project_name_latin_encoded(volume)
+        filename = '%s_%s_%s.zip' % (name, ty, _format)
+        filename = secure_filename(filename)
+        return filename
