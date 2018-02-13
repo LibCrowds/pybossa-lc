13624d3665debb846df1cd6298d9399c5ca256cd
Alex Mendes <alexanderhmendes@gmail.com>
2018-02-13 12:59:58 +0000

    Remove annotations blueprint

--- pybossa_lc/__init__.py
+++ pybossa_lc/__init__.py
@@ -42,13 +42,11 @@ class PyBossaLC(Plugin):
         from .api.users import BLUEPRINT as users
         from .api.categories import BLUEPRINT as categories
         from .api.templates import BLUEPRINT as templates
-        from .api.annotations import BLUEPRINT as annos
         app.register_blueprint(analysis, url_prefix='/libcrowds/analysis')
         app.register_blueprint(projects, url_prefix='/libcrowds/projects')
         app.register_blueprint(users, url_prefix='/libcrowds/users')
         app.register_blueprint(categories, url_prefix='/libcrowds/categories')
         app.register_blueprint(templates, url_prefix='/libcrowds/templates')
-        app.register_blueprint(annos, url_prefix='/libcrowds/annotations')
 
     def setup_iiif_importer(self):
         """Setup the IIIF manifest importer."""
