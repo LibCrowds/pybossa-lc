f48efcf03b515b15cda2ddbf95253fa15d900d84
Alexander Mendes <alexanderhmendes@gmail.com>
2018-04-11 16:30:51 +0100

    Fix anno endpoint

--- pybossa_lc/api/annotations.py
+++ pybossa_lc/api/annotations.py
@@ -2,7 +2,7 @@
 """API annotations module for pybossa-lc."""
 
 import json
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, current_app
 
 from ..cache import annotations as annotations_cache
 
@@ -21,8 +21,9 @@ def json_response(data):
 @BLUEPRINT.route('/<annotation_id>')
 def get(annotation_id):
     """Return an annotation."""
-    anno = annotations_cache.get(request.url)
-    print request.path
+    spa_server_name = current_app.config.get('SPA_SERVER_NAME')
+    full_id = '{0}/lc/annotations/{1}'.format(spa_server_name, annotation_id)
+    anno = annotations_cache.get(full_id)
     if not anno:
         abort(404)
 
