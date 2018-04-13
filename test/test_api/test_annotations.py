d033632f1a27f6b3477d6c694c8d5cecd293d87e
Alexander Mendes <alexanderhmendes@gmail.com>
2018-04-13 18:11:40 +0100

    Add test

--- test/test_api/test_annotations.py
+++ test/test_api/test_annotations.py
@@ -185,7 +185,7 @@ class TestAnnotationsApi(web.Helper):
 
         spa_server_name = flask_app.config.get('SPA_SERVER_NAME')
         annos_page = annotations[per_page * (page - 1):per_page * page]
-        assert_equal(len(annos_page), per_page)
+        assert_equal(len(annos_page), min(len(annotations), per_page))
         assert_equal(json.loads(res.data), {
             "@context": "http://www.w3.org/ns/anno.jsonld",
             "id": "{0}{1}".format(spa_server_name, endpoint),
@@ -196,6 +196,51 @@ class TestAnnotationsApi(web.Helper):
                 "total": len(annotations)
             },
             "next": "{0}{1}/{2}".format(spa_server_name, url_base, page + 1),
+            "startIndex": 0,
+            "items": annos_page
+        })
+
+    @with_context
+    def test_annotation_volume_page_returned_with_motivation(self):
+        """Test Annotation Page returned for a volume and motivation."""
+        self.register()
+        owner = self.user_repo.get(1)
+        vol = dict(id='foo', name='bar', short_name='bar', importer='baz')
+        category = CategoryFactory(info=dict(volumes=[vol]))
+        project = ProjectFactory(category=category, owner=owner,
+                                 info=dict(volume_id=vol['id']))
+        task = TaskFactory(n_answers=1, project=project)
+        TaskRunFactory.create(task=task, project=project, user=owner)
+        result = self.result_repo.get_by(task_id=task.id)
+        per_page = flask_app.config.get('ANNOTATIONS_PER_PAGE')
+        motivation = 'tagging'
+        valid_anno = self.create_annotation(motivation)
+        annotations = [
+            self.create_annotation('commenting'),
+            valid_anno
+        ]
+        result.info = dict(annotations=annotations)
+        self.result_repo.update(result)
+
+        url_base = '/lc/annotations/wa/collection/volume/{}'.format(vol['id'])
+        page = 1
+        endpoint = '{0}/{1}'.format(url_base, page)
+        res = self.app_get_json(endpoint + '?motivation={}'.format(motivation))
+
+        self.check_response(res)
+
+        spa_server_name = flask_app.config.get('SPA_SERVER_NAME')
+        annos_page = [valid_anno]
+        assert_equal(len(annos_page), min(len([valid_anno]), per_page))
+        assert_equal(json.loads(res.data), {
+            "@context": "http://www.w3.org/ns/anno.jsonld",
+            "id": "{0}{1}".format(spa_server_name, endpoint),
+            "type": "AnnotationPage",
+            "partOf": {
+                "id": "{0}{1}".format(spa_server_name, url_base),
+                "label": "{0} Annotations".format(vol['name']),
+                "total": len([valid_anno])
+            },
             "startIndex": 0,
             "items": annos_page
         })
