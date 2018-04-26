da2582c5e3730201de9fccb222d09f4249ee27f6
Alexander Mendes <alexanderhmendes@gmail.com>
2018-04-26 16:19:27 +0100

    Fix tests

--- test/test_api/test_annotations.py
+++ test/test_api/test_annotations.py
@@ -11,13 +11,6 @@ from factories import ProjectFactory, TaskFactory, TaskRunFactory
 from factories import CategoryFactory
 from pybossa.repositories import ResultRepository, UserRepository
 
-try:
-    from urllib import urlencode
-except ImportError:  # py3
-    from urllib.parse import urlencode
-
-from pybossa_lc.api import annotations as annotations_api
-
 
 class TestAnnotationsApi(web.Helper):
 
@@ -85,6 +78,7 @@ class TestAnnotationsApi(web.Helper):
         task = TaskFactory(n_answers=1, project=project)
         TaskRunFactory.create(task=task, project=project, user=owner)
         result = self.result_repo.get_by(task_id=task.id)
+
         anno = self.create_annotation()
         result.info = dict(annotations=[anno])
         self.result_repo.update(result)
@@ -100,23 +94,34 @@ class TestAnnotationsApi(web.Helper):
     @with_context
     def test_annotation_collection_returned(self):
         """Test Annotation Collection returned."""
+        self.register()
+        owner = self.user_repo.get(1)
+        category = CategoryFactory()
+        project = ProjectFactory(category=category, owner=owner)
+        task = TaskFactory(n_answers=1, project=project)
+        TaskRunFactory.create(task=task, project=project, user=owner)
+        result = self.result_repo.get_by(task_id=task.id)
+
         per_page = flask_app.config.get('ANNOTATIONS_PER_PAGE')
         annotations = [self.create_annotation()] * (per_page + 1)
-        count = len(annotations)
-        entity = ProjectFactory()
-        url_base = 'http://example.com'
-        query_str = 'foo=bar'
+        result.info = dict(annotations=annotations)
+        self.result_repo.update(result)
 
-        anno_coll = annotations_api.get_anno_collection(count, entity,
-                                                        url_base,
-                                                        query_str=query_str)
-        id_uri = '{0}/{1}'.format(url_base, entity.id)
-        assert_dict_equal(anno_coll, {
+        query_str = 'foo=bar'
+        endpoint = '/lc/annotations/wa/collection/{}'.format(category.id)
+        res = self.app_get_json(endpoint + '?' + query_str)
+
+        self.check_response(res)
+
+        spa_server_name = flask_app.config.get('SPA_SERVER_NAME')
+        id_uri = spa_server_name + endpoint
+
+        assert_dict_equal(json.loads(res.data), {
             "@context": "http://www.w3.org/ns/anno.jsonld",
             "id": "{0}?{1}".format(id_uri, query_str),
             "type": "AnnotationCollection",
-            "label": u"{0} Annotations".format(entity.name),
-            "total": count,
+            "label": u"{0} Annotations".format(category.name),
+            "total": len(annotations),
             "first": "{0}/1?{1}".format(id_uri, query_str),
             "last": "{0}/2?{1}".format(id_uri, query_str)
         })
@@ -124,72 +129,100 @@ class TestAnnotationsApi(web.Helper):
     @with_context
     def test_annotation_page_returned(self):
         """Test Annotation Page returned."""
+        self.register()
+        owner = self.user_repo.get(1)
+        category = CategoryFactory()
+        project = ProjectFactory(category=category, owner=owner)
+        task = TaskFactory(n_answers=1, project=project)
+        TaskRunFactory.create(task=task, project=project, user=owner)
+        result = self.result_repo.get_by(task_id=task.id)
+
         per_page = flask_app.config.get('ANNOTATIONS_PER_PAGE')
         annotations = [self.create_annotation()] * (per_page + 1)
-        count = len(annotations)
-        entity = ProjectFactory()
-        url_base = 'http://example.com'
+        result.info = dict(annotations=annotations)
+        self.result_repo.update(result)
+
         page = 1
         query_str = 'foo=bar'
+        url_base = '/lc/annotations/wa/collection/{0}'.format(category.id)
+        endpoint = '{0}/{1}'.format(url_base, page)
+        res = self.app_get_json(endpoint + '?' + query_str)
 
-        anno_page = annotations_api.get_anno_page(annotations, count, entity,
-                                                  url_base, page,
-                                                  query_str=query_str)
-        id_uri = '{0}/{1}'.format(url_base, entity.id)
+        spa_server_name = flask_app.config.get('SPA_SERVER_NAME')
+        coll_id_uri = spa_server_name + url_base
 
-        assert_dict_equal(anno_page, {
+        assert_dict_equal(json.loads(res.data), {
             "@context": "http://www.w3.org/ns/anno.jsonld",
-            "id": "{0}/{1}?{2}".format(id_uri, page, query_str),
+            "id": "{0}/{1}?{2}".format(coll_id_uri, page, query_str),
             "type": "AnnotationPage",
             "partOf": {
-                "id": "{0}?{1}".format(id_uri, query_str),
-                "label": u"{0} Annotations".format(entity.name),
-                "total": count
+                "id": "{0}?{1}".format(coll_id_uri, query_str),
+                "label": u"{0} Annotations".format(category.name),
+                "total": len(annotations)
             },
-            "next": "{0}/{1}?{2}".format(id_uri, page + 1, query_str),
+            "next": "{0}/{1}?{2}".format(coll_id_uri, page + 1, query_str),
             "startIndex": 0,
-            "items": annotations
+            "items": annotations[:per_page * page]
         })
 
     @with_context
-    def test_none_returned_if_annotation_page_empty(self):
-        """Test None returned if Annotation Page is empty."""
-        per_page = flask_app.config.get('ANNOTATIONS_PER_PAGE')
-        annotations = []
-        count = per_page - 1
-        entity = ProjectFactory()
-        url_base = 'http://example.com'
-        page = 2
+    def test_404_if_annotation_page_empty(self):
+        """Test 404 returned if Annotation Page is empty."""
+        self.register()
+        owner = self.user_repo.get(1)
+        category = CategoryFactory()
+        project = ProjectFactory(category=category, owner=owner)
+        task = TaskFactory(n_answers=1, project=project)
+        TaskRunFactory.create(task=task, project=project, user=owner)
+        result = self.result_repo.get_by(task_id=task.id)
 
-        anno_page = annotations_api.get_anno_page(annotations, count, entity,
-                                                  url_base, page)
-        assert_equal(anno_page, None)
+        per_page = flask_app.config.get('ANNOTATIONS_PER_PAGE')
+        annotations = [self.create_annotation()] * per_page
+        result.info = dict(annotations=annotations)
+        self.result_repo.update(result)
+
+        page = len(annotations) + 1
+        url_base = '/lc/annotations/wa/collection/{0}'.format(category.id)
+        endpoint = '{0}/{1}'.format(url_base, page)
+        res = self.app_get_json(endpoint)
+
+        assert_equal(res.status_code, 404)
 
     @with_context
     def test_annotation_page_returned_with_iris_only(self):
         """Test Annotation Page returned with IRIs only."""
+        self.register()
+        owner = self.user_repo.get(1)
+        category = CategoryFactory()
+        project = ProjectFactory(category=category, owner=owner)
+        task = TaskFactory(n_answers=1, project=project)
+        TaskRunFactory.create(task=task, project=project, user=owner)
+        result = self.result_repo.get_by(task_id=task.id)
+
         per_page = flask_app.config.get('ANNOTATIONS_PER_PAGE')
         annotations = [self.create_annotation()] * (per_page + 1)
-        count = len(annotations)
-        entity = ProjectFactory()
-        url_base = 'http://example.com'
+        result.info = dict(annotations=annotations)
+        self.result_repo.update(result)
+
         page = 1
+        query_str = 'iris=1'
+        url_base = '/lc/annotations/wa/collection/{0}'.format(category.id)
+        endpoint = '{0}/{1}'.format(url_base, page)
+        res = self.app_get_json(endpoint + '?' + query_str)
 
-        anno_page = annotations_api.get_anno_page(annotations, count, entity,
-                                                  url_base, page, iris=True)
-        id_uri = '{0}/{1}'.format(url_base, entity.id)
-        items = [anno['id'] for anno in annotations]
+        spa_server_name = flask_app.config.get('SPA_SERVER_NAME')
+        coll_id_uri = spa_server_name + url_base
 
-        assert_dict_equal(anno_page, {
+        assert_dict_equal(json.loads(res.data), {
             "@context": "http://www.w3.org/ns/anno.jsonld",
-            "id": "{0}/{1}".format(id_uri, page),
+            "id": "{0}/{1}?{2}".format(coll_id_uri, page, query_str),
             "type": "AnnotationPage",
             "partOf": {
-                "id": id_uri,
-                "label": u"{0} Annotations".format(entity.name),
-                "total": count
+                "id": "{0}?{1}".format(coll_id_uri, query_str),
+                "label": u"{0} Annotations".format(category.name),
+                "total": len(annotations)
             },
-            "next": "{0}/{1}".format(id_uri, page + 1),
+            "next": "{0}/{1}?{2}".format(coll_id_uri, page + 1, query_str),
             "startIndex": 0,
-            "items": items
+            "items": [anno['id'] for anno in annotations[:per_page * page]]
         })
