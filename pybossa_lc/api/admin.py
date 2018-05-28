591d6e12d54f04de646dd81d759a5117fe464d59
Alex Mendes <alexanderhmendes@gmail.com>
2018-05-28 15:11:23 +0100

    Rename /project-template > /templates

--- pybossa_lc/api/admin.py
+++ pybossa_lc/api/admin.py
@@ -63,7 +63,7 @@ def analyse_empty_results(category_id):
 
 @login_required
 @admin_required
-@BLUEPRINT.route('/project-template', methods=['GET'])
+@BLUEPRINT.route('/templates', methods=['GET'])
 def  get_template_framework():
     """Return the fields required for different parts of a template.
 
