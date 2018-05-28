e1f08ff751ef3b5001c59eceb33a966636e7a57b
Alex Mendes <alexanderhmendes@gmail.com>
2018-05-29 00:12:51 +0100

    Change empty fields_schema to dict

--- pybossa_lc/api/admin.py
+++ pybossa_lc/api/admin.py
@@ -92,7 +92,7 @@ def get_template_framework():
             'tag': '',
             'objective': '',
             'guidance': '',
-            'fields_schema': {}  # Populated with vue-multiselect fields
+            'fields_schema': []  # Populated with vue-multiselect fields
         }
     }
 
